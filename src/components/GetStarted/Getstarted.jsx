94f7107b9c0f178efbeb8f211478e6bb645570f6
Kumar Shivam <lookup.shivam@gmail.com>
2024-06-26 12:43:08 +0530

    my commit

---
+++ src/components/GetStarted/Getstarted.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Getstarted.css'
+
+const Getstarted = () => {
+  return (
+    <section className='g-wrapper'>
+<div class="paddings innerWidth g-container">
+<div class="flexColCenter inner-container">
+<span className='primaryText'>Get Started with Homyz</span>
+<span className='secondaryText'>Subscribe and find super attractive price quotes from us.
+<br/>
+Find your residence soon
+</span>
+<button className='button'>
+    <a href='mailto:lookup.shivam@gmail.com'>Get Started</a>
+</button>
+</div>
+</div>
+    </section>
+  )
+}
+
+export default Getstarted
